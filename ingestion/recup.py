2f0d862006c611dbbfd5c3401bade1ca24cdbbe1
germa <sacha.germanier@students.hevs.ch>
2026-02-26 15:57:16 +0100

    SMB check

--- ingestion/recup.py
+++ ingestion/recup.py
@@ -1,1 +1,33 @@
 # Async sensor polling — fetches Jimmy & Jérémie JSON endpoints concurrently, writes raw files to Bronze
+
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+#######################
+### SMB Apartments .json file
+
+class NewFileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            print(f"New file detected : {event.src_path}")
+            time.sleep(1) 
+            # Copie avec gestion d'erreur
+    
+
+# UNC SMB
+path = r""
+
+
+observer = Observer()
+observer.schedule(NewFileHandler(), path=path, recursive=False)
+observer.start()
+
+try:
+    while True:
+	# Run in background, main thread can do other things or sleep
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
